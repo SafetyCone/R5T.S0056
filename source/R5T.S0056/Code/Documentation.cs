0511e4133a98e76facad9a6c93c40fef67a62d81
davidcoats <david@getsafetycone.com>
2022-12-05 12:19:27 -0800

    Initial commit.

---
+++ source/R5T.S0056/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.S0056
+{
+	/// <summary>
+	/// Path-related scripts.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.S0056/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.S0056
+Path-related scripts.
